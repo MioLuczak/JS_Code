d654d0d9194e650376ab31cdb8919f131cee9055
Mio Łuczak <mioluczak@gmail.com>
2024-05-16 09:22:28 +0200

    Generated random array

---
+++ scriptSearchingWithFilter.js
@@ -0,0 +1,63 @@
+// Generated random array from JSONGenerator
+products: [
+  {
+    name: "Unbranded Metal Bacon",
+    price: 67.0,
+    available: false,
+    stock: 120,
+  },
+  {
+    name: "Luxurious Concrete Bike",
+    price: 283.0,
+    available: true,
+    stock: 105,
+  },
+  {
+    name: "Handmade Metal Table",
+    price: 550.0,
+    available: false,
+    stock: 91,
+  },
+  {
+    name: "Recycled Plastic Chips",
+    price: 489.0,
+    available: true,
+    stock: 122,
+  },
+  {
+    name: "Awesome Plastic Bike",
+    price: 359.0,
+    available: true,
+    stock: 97,
+  },
+  {
+    name: "Oriental Metal Table",
+    price: 190.0,
+    available: true,
+    stock: 110,
+  },
+  {
+    name: "Ergonomic Metal Shirt",
+    price: 419.0,
+    available: false,
+    stock: 62,
+  },
+  {
+    name: "Oriental Wooden Soap",
+    price: 719.0,
+    available: true,
+    stock: 58,
+  },
+  {
+    name: "Handmade Wooden Bacon",
+    price: 285.0,
+    available: false,
+    stock: 122,
+  },
+  {
+    name: "Sleek Fresh Car",
+    price: 872.0,
+    available: false,
+    stock: 62,
+  },
+];
