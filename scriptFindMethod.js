f8cca50b5c3e6cbd36de350031cf5c54368413b0
Mio Łuczak <mioluczak@gmail.com>
2024-05-19 20:57:07 +0200

    function that returns array with even numbers

--- scriptFindMethod.js
+++ scriptFindMethod.js
@@ -1,2 +1,9 @@
 // establishing array  - Random number generated
 const numbers = [-7, 7, -5, 11, 6, 16, -14, 15, 16, -13];
+
+function findEvenNumbers(arr) {
+  const result = arr.filter((element) => element % 2 === 0);
+  return result;
+}
+
+console.log(findEvenNumbers(numbers));
