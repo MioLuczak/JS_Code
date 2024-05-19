aadff4c1e95b88237cfdda236b57fb43a618454c
Mio Łuczak <mioluczak@gmail.com>
2024-05-19 21:16:15 +0200

    Console.log removed; Message if function don't find any even number

--- scriptFindMethod.js
+++ scriptFindMethod.js
@@ -2,8 +2,12 @@
 const numbers = [-7, 7, -5, 11, 6, 16, -14, 15, 16, -13];
 
 function findEvenNumbers(arr) {
-  const result = arr.filter((element) => element % 2 === 0);
-  return result;
+  const result = Array.from(arr.filter((element) => element % 2 === 0));
+  if (result.length > 0) {
+    return result;
+  } else {
+    return 'Not found';
+  }
 }
-
+  
 console.log(findEvenNumbers(numbers));
