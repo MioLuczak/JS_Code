2dcfb27775bdd584d5139489039b19718f0cdf92
Mio Łuczak <mioluczak@gmail.com>
2024-05-20 11:46:03 +0200

    Creating Array of Goods

---
+++ scriptObjectSearching.js
@@ -0,0 +1,18 @@
+// Creating products array
+const groceryStoreProducts = [
+  { name: 'Garlic', category: 'Vegetables', isAvailable: true },
+  { name: 'Onion', category: 'Vegetables', isAvailable: true },
+  { name: 'Potatoes', category: 'Vegetables', isAvailable: false },
+  { name: 'Eggs', category: 'Dairy', isAvailable: true },
+  { name: 'Butter', category: 'Dairy', isAvailable: false },
+  { name: 'Milk', category: 'Dairy', isAvailable: true },
+  { name: 'Sliced Cheese', category: 'Dairy', isAvailable: true },
+  { name: 'Sour Cream', category: 'Dairy', isAvailable: true },
+  { name: 'Biscuits', category: 'Bakery', isAvailable: true },
+  { name: 'Pie', category: 'Bakery', isAvailable: false },
+  { name: 'Pomegranate', category: 'Fruits', isAvailable: true },
+  { name: 'Pear', category: 'Fruits', isAvailable: true },
+  { name: 'Apple', category: 'Fruits', isAvailable: false },
+  { name: 'Chicken breast', category: 'Meat', isAvailable: true },
+  { name: 'Pork', category: 'Meat', isAvailable: true },
+];
