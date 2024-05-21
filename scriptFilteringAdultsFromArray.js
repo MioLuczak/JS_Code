f915c6ecd26712c98a2a5641f53eb18b62662aab
Mio Łuczak <mioluczak@gmail.com>
2024-05-21 09:40:28 +0200

    Creating Array Objects of people

---
+++ scriptFilteringAdultsFromArray.js
@@ -0,0 +1,83 @@
+// Creating Array of random people
+const people = [
+  {
+    id: "664c4e9f664cf2fab2183731",
+    isActive: true,
+    age: 23,
+    firstname: "Rivas",
+    lastname: "Castaneda",
+    email: "rivascastaneda@poochies.com"
+  },
+  {
+    id: "664c4e9ffd8ca93ad45a6b1c",
+    isActive: true,
+    age: 32,
+    firstname: "Vivian",
+    lastname: "Weber",
+    email: "vivianweber@poochies.com"
+  },
+  {
+    id: "664c4e9fadc91499cb0cfffb",
+    isActive: false,
+    age: 30,
+    firstname: "Rene",
+    lastname: "Moses",
+    email: "renemoses@poochies.com"
+  },
+  {
+    id: "664c4e9f25d428ed4ca82217",
+    isActive: false,
+    age: 22,
+    firstname: "Vance",
+    lastname: "Haney",
+    email: "haneyvance@poochies.com"
+  },
+  {
+    id: "664c4e9f36459e4dbcab375f",
+    isActive: true,
+    age: 39,
+    firstname: "Clayton",
+    lastname: "Alisha",
+    email: "alishaclayton@poochies.com"
+  },
+  {
+    id: "664c4e9f9fbe7e4a6c44676f",
+    isActive: false,
+    age: 36,
+    firstname: "Patterson",
+    lastname: "Petty",
+    email: "pettypatterson@poochies.com"
+  },
+  {
+    id: "664c4e9f86dd113f5af2e736",
+    isActive: false,
+    age: 33,
+    firstname: "Terry",
+    lastname: "Beck",
+    email: "beckterry@poochies.com"
+  },
+  {
+    id: "664c4e9f2588ca93b25c9fa7",
+    isActive: true,
+    age: 38,
+    firstname: "Vaughan",
+    lastname: "Landry",
+    email: "landryvaughan@poochies.com"
+  },
+  {
+    id: "664c4e9ff4851d030f4529a8",
+    isActive: true,
+    age: 32,
+    firstname: "Richards",
+    lastname: "Cherry",
+    email: "cherryrichards@poochies.com"
+  },
+  {
+    id: "664c4e9f3ebf841b23d8638d",
+    isActive: false,
+    age: 38,
+    firstname: "Taylor",
+    lastname: "House",
+    email: "housetaylor@poochies.com"
+  }
+]
